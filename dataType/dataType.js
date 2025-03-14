8603f88924ce14e5b6694112f65f00fc8eca7333
udeshkumar12 <udeshk949@gmail.com>
2025-03-13 20:49:37 -0400

    all bout data types

---
+++ dataType/dataType.js
@@ -0,0 +1,58 @@
+// JavaScript provides different data types to hold different types of values. There are two types of data types in JavaScript.
+
+// 1 Primitive data type ;
+// 2 Non-primitive (reference) data type ;
+
+// JavaScript primitive data types
+// There are five types of primitive data types in JavaScript. They are as follows:
+
+// Data Type	Description
+// String	represents sequence of characters e.g. "hello"
+// Number	represents numeric values e.g. 100
+// Boolean	represents boolean value either false or true
+// Undefined	represents undefined value
+// Null	represents null i.e. no value at all
+
+// JavaScript non-primitive data types
+// The non-primitive data types are as follows:
+
+// Data Type	Description
+// Object	represents instance through which we can access members
+// Array	represents group of similar values
+// RegExp	represents regular expression
+
+
+// string data types
+var a = "Hello World";
+console.log(a);
+console.log(typeof a);
+
+/* Number Data Type */
+var b = 25;
+console.log(b);
+console.log(typeof b);
+
+/* Boolean Data Type */
+var c = true;
+console.log(c);
+console.log(typeof c);
+
+/* Array Data Type */
+var d = ["HTML", "CSS", "JS"];
+console.log(d);
+console.log(typeof d);
+
+// * object data type
+var x = { first: "Jane", last: "Doe" };
+console.log(x);
+console.log(typeof x);
+
+/* Null Data Type */
+var y = null;
+console.log(y);
+console.log(typeof y);
+
+/* undefined Data Type */
+var z;
+console.log(z);
+console.log(typeof z);
