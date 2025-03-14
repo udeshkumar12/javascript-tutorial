0d727b0cdfa445be0e468cbe5cad1bfb50bbb91c
udeshkumar12 <udeshk949@gmail.com>
2025-03-13 20:12:41 -0400

    all about variable

---
+++ variable/variable.js
@@ -0,0 +1,47 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+
+// Using var
+
+// Using let
+
+// Using const
+
+
+// Var Keyword:
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+
+// The let and const keywords were added to JavaScript in 2015.
+
+// The var keyword should only be used in code written for older browsers.
+
+var x = "Hello";
+var y = "World";
+console.log(x + y);
+
+
+// Let Variable:
+// Variables defined with let cannot be Redeclared
+
+// Variables defined with let must be Declared before use
+
+// Variables defined with let have Block Scope
+
+
+
+let z = "Hello World";
+console.log(z);
+
+
+// Const Variable:
+// Variables defined with const cannot be Redeclared
+
+// Variables defined with const cannot be Reassigned
+
+// Variables defined with const have Block Scope
+
+
+const second = "Hello World";
+console.log(second); 
