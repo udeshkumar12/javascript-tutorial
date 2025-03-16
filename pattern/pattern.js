5668fa6a9c689dbacfbdcd31b6599b5cbe79ca1e
udeshkumar12 <udeshk949@gmail.com>
2025-03-16 11:26:28 -0400

    pattern

---
+++ pattern/pattern.js
@@ -0,0 +1,65 @@
+// pattern 1
+// for (let i=1; i<= 5; i++) {
+//     for (let i=1; i<=5; i++){
+//     document.writeln(i)
+//     }
+//  document.writeln("<br>");
+// };
+// pattern 2
+// for (let j=1; j<= 5; j++) {
+//     for (let i=1; i<=j; i++){
+//     document.writeln(i)
+//     }
+//  document.writeln("<br>");
+// };
+// pattern 3
+// for (let j=1; j<= 5; j++) {
+//     for (let i=1; i<=j; i++){
+//     document.writeln(String.fromCharCode((i+64)));
+//     }
+//  document.writeln("<br>");
+// };
+// pattern 4
+// for (let j = 1; j <= 5; j++) {
+//   for (let i = 1; i <= j; i++) {
+//     document.writeln("*");
+//   }
+//   document.writeln("<br>");
+// };
+// pattern 5
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.writeln(i);
+//   }
+//   document.writeln("<br>");
+// };
+// pattern 6
+// for (let j=1; j<= 5; j++) {
+//     for (let i=1; i<=j; i++){
+//     document.writeln(String.fromCharCode((j+64)));
+//     }
+//  document.writeln("<br>");
+// };
+// pattern 7
+// for (let i = 5; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     document.writeln(j);
+//   }
+//   document.writeln("<br>");
+// };
+// pattern 8 
+// for (let i = 5; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     document.writeln(i);
+//   }
+//   document.writeln("<br>");
+// };
+// pattern 9
+for (let i = 5; i >= 1; i--) {
+  for (let j = 5; j >(5-i); j--) {
+    document.writeln(j);
+  }
+  document.writeln("<br>");
+};
+
+
